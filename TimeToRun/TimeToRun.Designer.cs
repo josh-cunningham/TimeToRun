ba8468edbd5c7c26b7d5e5bb8c400bd728a8a3af
josh-cunningham <josh_cunningham_@hotmail.co.uk>
2015-07-01 21:45:17 +0100

    Extended HCI to enable compilation of more complex code snippets
    Added textboxes for using statements, variable declaration,
    initialisation and code that will be timed (when functionality is
    extended).
    Also added default text in textboxes to indicate what code should go
    where, default text intelligently disappears and reappears when focus is
    got and lost respectively.

--- TimeToRun/TimeToRun.Designer.cs
+++ TimeToRun/TimeToRun.Designer.cs
@@ -28,58 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
             this.OutputTextBox = new System.Windows.Forms.TextBox();
             this.CompileBtn = new System.Windows.Forms.Button();
+            this.usingStatementsTextBox = new System.Windows.Forms.TextBox();
+            this.codeToTimeGroupBox = new System.Windows.Forms.GroupBox();
+            this.initializationGroupBox = new System.Windows.Forms.GroupBox();
+            this.initializationTextBox = new System.Windows.Forms.TextBox();
+            this.variablesTextBox = new System.Windows.Forms.TextBox();
+            this.codeToTimeGroupBox.SuspendLayout();
+            this.initializationGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // InputTextBox
+            // inputTextBox
             // 
-            this.InputTextBox.Location = new System.Drawing.Point(20, 21);
-            this.InputTextBox.Multiline = true;
-            this.InputTextBox.Name = "InputTextBox";
-            this.InputTextBox.Size = new System.Drawing.Size(311, 123);
-            this.InputTextBox.TabIndex = 0;
+            this.inputTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.inputTextBox.Location = new System.Drawing.Point(6, 19);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(326, 183);
+            this.inputTextBox.TabIndex = 0;
             // 
             // OutputTextBox
             // 
             this.OutputTextBox.Enabled = false;
-            this.OutputTextBox.Location = new System.Drawing.Point(20, 185);
+            this.OutputTextBox.Location = new System.Drawing.Point(338, 19);
             this.OutputTextBox.Multiline = true;
             this.OutputTextBox.Name = "OutputTextBox";
-            this.OutputTextBox.Size = new System.Drawing.Size(310, 122);
+            this.OutputTextBox.Size = new System.Drawing.Size(332, 183);
             this.OutputTextBox.TabIndex = 1;
             // 
             // CompileBtn
             // 
-            this.CompileBtn.Location = new System.Drawing.Point(20, 150);
+            this.CompileBtn.Location = new System.Drawing.Point(18, 400);
             this.CompileBtn.Name = "CompileBtn";
-            this.CompileBtn.Size = new System.Drawing.Size(311, 29);
+            this.CompileBtn.Size = new System.Drawing.Size(664, 45);
             this.CompileBtn.TabIndex = 2;
             this.CompileBtn.Text = "Compile";
             this.CompileBtn.UseVisualStyleBackColor = true;
             this.CompileBtn.Click += new System.EventHandler(this.CompileBtn_Click);
             // 
+            // usingStatementsTextBox
+            // 
+            this.usingStatementsTextBox.Location = new System.Drawing.Point(6, 19);
+            this.usingStatementsTextBox.Multiline = true;
+            this.usingStatementsTextBox.Name = "usingStatementsTextBox";
+            this.usingStatementsTextBox.Size = new System.Drawing.Size(205, 139);
+            this.usingStatementsTextBox.TabIndex = 3;
+            // 
+            // codeToTimeGroupBox
+            // 
+            this.codeToTimeGroupBox.Controls.Add(this.OutputTextBox);
+            this.codeToTimeGroupBox.Controls.Add(this.inputTextBox);
+            this.codeToTimeGroupBox.Location = new System.Drawing.Point(12, 186);
+            this.codeToTimeGroupBox.Name = "codeToTimeGroupBox";
+            this.codeToTimeGroupBox.Size = new System.Drawing.Size(676, 208);
+            this.codeToTimeGroupBox.TabIndex = 4;
+            this.codeToTimeGroupBox.TabStop = false;
+            this.codeToTimeGroupBox.Text = "Code To Time";
+            // 
+            // initializationGroupBox
+            // 
+            this.initializationGroupBox.Controls.Add(this.initializationTextBox);
+            this.initializationGroupBox.Controls.Add(this.variablesTextBox);
+            this.initializationGroupBox.Controls.Add(this.usingStatementsTextBox);
+            this.initializationGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.initializationGroupBox.Name = "initializationGroupBox";
+            this.initializationGroupBox.Size = new System.Drawing.Size(676, 164);
+            this.initializationGroupBox.TabIndex = 5;
+            this.initializationGroupBox.TabStop = false;
+            this.initializationGroupBox.Text = "Code Initialization";
+            // 
+            // initializationTextBox
+            // 
+            this.initializationTextBox.Location = new System.Drawing.Point(459, 19);
+            this.initializationTextBox.Multiline = true;
+            this.initializationTextBox.Name = "initializationTextBox";
+            this.initializationTextBox.Size = new System.Drawing.Size(211, 139);
+            this.initializationTextBox.TabIndex = 5;
+            // 
+            // variablesTextBox
+            // 
+            this.variablesTextBox.Location = new System.Drawing.Point(217, 19);
+            this.variablesTextBox.Multiline = true;
+            this.variablesTextBox.Name = "variablesTextBox";
+            this.variablesTextBox.Size = new System.Drawing.Size(236, 139);
+            this.variablesTextBox.TabIndex = 4;
+            // 
             // TimeToRun
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(349, 322);
+            this.ClientSize = new System.Drawing.Size(700, 457);
+            this.Controls.Add(this.initializationGroupBox);
+            this.Controls.Add(this.codeToTimeGroupBox);
             this.Controls.Add(this.CompileBtn);
-            this.Controls.Add(this.OutputTextBox);
-            this.Controls.Add(this.InputTextBox);
             this.Name = "TimeToRun";
-            this.Text = "Time To Run v0.001";
+            this.Text = "Time To Run C# v0.002";
+            this.codeToTimeGroupBox.ResumeLayout(false);
+            this.codeToTimeGroupBox.PerformLayout();
+            this.initializationGroupBox.ResumeLayout(false);
+            this.initializationGroupBox.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox inputTextBox;
         private System.Windows.Forms.TextBox OutputTextBox;
         private System.Windows.Forms.Button CompileBtn;
+        private System.Windows.Forms.TextBox usingStatementsTextBox;
+        private System.Windows.Forms.GroupBox codeToTimeGroupBox;
+        private System.Windows.Forms.GroupBox initializationGroupBox;
+        private System.Windows.Forms.TextBox initializationTextBox;
+        private System.Windows.Forms.TextBox variablesTextBox;
     }
 }
 
--- TimeToRun/TimeToRun.cs
+++ TimeToRun/TimeToRun.cs
@@ -1,38 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace TimeToRun
+﻿namespace TimeToRun
 {
+    using System;
     using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Drawing;
     using System.IO;
     using Microsoft.CSharp;
     using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
 
     public partial class TimeToRun : Form
     {
         private CSharpCodeProvider codeProvider;
-
+        
         private CompilerParameters compilerParameters;
+
+        Dictionary<TextBox, string> defaultTextsDictionary;
+
+        #region Constructors and Initialization methods
 
         public TimeToRun()
         {
-            InitializeComponent();
+            this.InitializeComponent();
 
-            InitializeInputTextBox();
+            this.InitializeInputTextBoxes();
 
-            InitializeCompiler();
+            this.InitializeCompiler();
         }
 
-        private void InitializeInputTextBox()
+        private void InitializeInputTextBoxes()
         {
-            this.InputTextBox.Text = "Console.WriteLine(\"Hello World!\"); Console.ReadKey();";
+            defaultTextsDictionary = new Dictionary<TextBox, string>();
+            defaultTextsDictionary.Add(this.usingStatementsTextBox, "Enter 'Using' statements here\r\n\r\nNote: If unchanged, this text will not be compiled");
+            defaultTextsDictionary.Add(this.variablesTextBox, "Declare you variables here\r\n\r\nNote: If unchanged, this text will not be compiled");
+            defaultTextsDictionary.Add(this.initializationTextBox, "Initialise your variables here\r\nInclude any other code that you don't want to be timed\r\n\r\nNote: If unchanged, this text will not be compiled");
+            defaultTextsDictionary.Add(this.inputTextBox, "Enter the code you want to be timed here\r\n\r\nNote: If unchanged, this text will not be compiled");
+
+            foreach (TextBox textBox in this.defaultTextsDictionary.Keys)
+            {
+                this.SetToDefaultText(textBox);
+                textBox.GotFocus += this.InputTextBox_GotFocus;
+                textBox.LostFocus += this.InputTextBox_LostFocus;
+            }
         }
 
         private void InitializeCompiler()
@@ -49,11 +60,46 @@ namespace TimeToRun
             codeProvider = new CSharpCodeProvider();
         }
 
+        #endregion
+
+        #region Get and Set text boxes
+
         private string GetCompilableString()
         {
-            return "public static class TestProgram { public static void Main() {"
-                + this.InputTextBox.Text
-                + "} }";
+            StringBuilder compilableStringBuilder = new StringBuilder();
+
+            compilableStringBuilder.Append(this.GetCompilableString(this.usingStatementsTextBox));
+            compilableStringBuilder.Append("static class TestProgram { public static void Main() {");
+            compilableStringBuilder.Append("TestClass test = new TestClass(); test.Initialize(); test.RunCode();");
+            compilableStringBuilder.Append("} } public class TestClass { ");
+            compilableStringBuilder.Append(this.GetCompilableString(this.variablesTextBox));
+            compilableStringBuilder.Append("public void Initialize() {");
+            compilableStringBuilder.Append(this.GetCompilableString(this.initializationTextBox));
+            compilableStringBuilder.Append("} public void RunCode() {");
+            compilableStringBuilder.Append(this.GetCompilableString(this.inputTextBox));
+            compilableStringBuilder.Append("} }");
+
+            return compilableStringBuilder.ToString();
+        }
+
+        private string GetCompilableString(TextBox textBox)
+        {
+            return (textBox.Text != this.defaultTextsDictionary[textBox]) ? textBox.Text : string.Empty;
+        }
+
+        private void SetToDefaultText(TextBox textBox)
+        {
+            textBox.Text = this.defaultTextsDictionary[textBox];
+            textBox.ForeColor = Color.Gray;
+        }
+
+        #endregion
+
+        #region Compilation Methods
+
+        private CompilerResults Compile(string code)
+        {
+            return codeProvider.CompileAssemblyFromSource(this.compilerParameters, new string[] { code });
         }
 
         private string CompilationReport(CompilerResults results)
@@ -75,10 +121,9 @@ namespace TimeToRun
             }
         }
 
-        private CompilerResults Compile(string code)
-        {
-            return codeProvider.CompileAssemblyFromSource(this.compilerParameters, new string[] { code });
-        }
+        #endregion
+
+        #region Form control events
 
         private void CompileBtn_Click(object sender, EventArgs e)
         {
@@ -86,5 +131,31 @@ namespace TimeToRun
 
             this.OutputTextBox.Text = this.CompilationReport(results);
         }
+        
+        private void InputTextBox_GotFocus(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox is TextBox
+                && textBox.Text == this.defaultTextsDictionary[textBox])
+            {
+                textBox.Text = string.Empty;
+                textBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void InputTextBox_LostFocus(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox != null
+                && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                SetToDefaultText(textBox);
+            }
+        }
+
+        #endregion
+
     }
 }
